9aa749d47f6dadf7dfd176d5dab735d4f48ee42a
Zacarias Becker <zacariasbecker@gmail.com>
2022-07-27 10:09:11 -0300

    Design, Back to top, filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,40 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import styles from './styles.module.css';
-
 import DATA from '../data/data';
-
 import Item from '../components/item/Item';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 const Home = () => {
-
   const [textInput, setTextInput] = useState('');
+
+  // const scrollToTop = () => {
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: 'smooth'
+  //   });
+  // };
+
+  ////////////////////////////////////
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+  ////////////////////////////////////
+
 
   function handleString(text) {
     text = text.toLowerCase();
@@ -24,6 +50,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
+        {showButton && (<div className={styles.scrollToTop} onClick={() => scrollToTop()}><KeyboardArrowUpIcon /></div>)}
         <input type="text" onChange={e => setTextInput(handleString(e.target.value))} className={styles.textInput} placeholder="município" />
         {DATA.filter(item => handleString(item.municipio.toLowerCase()).includes(textInput)).map(item => {
           return (
